9d6a123cf45b0d1a11a60b94ce3d0b9ec6cd404b
Johan Castillo <jcjohan2707@gmail.com>
2021-01-07 16:04:22 -0400

    React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 // Components
 import Navegation from './components/navegation/Navegation';
 
 function App() {
   return (
     <>
-      <Navegation />
-      <h1>Hello World</h1>
+      <Router>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+          <li>
+            <Link to="/about-us">About us</Link>
+          </li>
+          <li>
+            <Link to="/contact-us">Contact</Link>
+          </li>
+        </ul>
+      
+
+        <Route exact path="/">
+          <Navegation />
+          <h1>Hello World</h1>
+        </Route>
+
+        <Route exact path="/products">
+          <h1>Products</h1>
+        </Route>
+
+        <Route exact path="/about-us">
+          <h1>About us</h1>
+        </Route>
+
+        <Route exact path="/contact-us">
+          <h1>Contact Us</h1>
+        </Route>
+
+
+      </Router>
     </>
   );
 }
