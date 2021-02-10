c111c0c9fee34884b09370fd4d0b5148c61d540a
Johan Castillo <jcjohan2707@gmail.com>
2021-02-10 11:45:15 -0400

    Count Regressive

--- frontend/src/components/promotion-card/PromotionCard.jsx
+++ frontend/src/components/promotion-card/PromotionCard.jsx
@@ -4,8 +4,10 @@ import { Link } from 'wouter'
 
 const PromotionCard = () => {
   const [promotion, setPromotion] = useState({})
-  const [progress, setProgress] = useState(0) 
+  const [progress, setProgress] = useState(0)
+  const [countRegressive, setCountRegressive] = useState({})
 
+  // Get data
   useEffect(() => {
     try {
       const action = async () => {
@@ -22,14 +24,66 @@ const PromotionCard = () => {
     }
   }, [])
 
+  // Progress bar
   useEffect(() => {
     const operation = (promotion.already_sold * 100) / promotion.initial_amount
-    
+
     setProgress(parseInt(operation))
     console.log(progress)
   }, [promotion])
 
+  // Count regresive
+  useEffect(() => {
+    const getRemainTime = deadline => {
+      let now = new Date(),
+        remainTime = (new Date(deadline) - now + 1000) / 1000,
+        remainSeconds = ("0" + Math.floor(remainTime % 60)).slice(-2),
+        remainMinutes = ("0" + Math.floor(remainTime / 60 % 60)).slice(-2),
+        remainHours = ("0" + Math.floor(remainTime / 3600 % 24)).slice(-2),
+        remainDays = ("0" + Math.floor(remainTime / (3600 * 24))).slice(-2)
 
+      return {
+        remainTime,
+        remainSeconds,
+        remainMinutes,
+        remainHours,
+        remainDays
+      }
+    }
+
+    // console.log(getRemainTime('Feb 10 2021 15:45:41 GMT-0400'))
+
+    const countdown = (deadline, finalMessage) => {
+
+      const timerUpdate = setInterval(() => {
+        let time = getRemainTime(deadline)
+
+        // console.log(time)
+
+        setCountRegressive({
+          remainDays: time.remainDays,
+          remainHours: time.remainHours, 
+          remainMinutes: time.remainMinutes,
+          remainSeconds: time.remainSeconds
+        })
+
+        
+
+        if (time.remainTime <= 1) {
+          clearInterval(timerUpdate)
+          setCountRegressive(finalMessage)
+        }
+
+      }, 1000)
+    }
+
+    console.log(promotion.date_limit)
+    countdown('Feb 14 2021 12:45:41 GMT-0400')
+    
+  }, [promotion])
+  
+
+  // Return View
   return (
     <div className="col-md-auto mb-6 mb-md-0">
       <div className="p-3 border border-width-2 border-primary borders-radius-20 bg-white min-width-370">
@@ -66,8 +120,8 @@ const PromotionCard = () => {
             <span>Vendidos: <strong>{promotion.already_sold}</strong></span>
           </div>
 
-          <div class="progress" style={{height: '1.2rem', fontSize: '14px'}}>
-            <div class="progress-bar bg-primary" role="progressbar" style={{width: `${progress}%`}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+          <div class="progress" style={{ height: '1.2rem', fontSize: '14px' }}>
+            <div class="progress-bar bg-primary" role="progressbar" style={{ width: `${progress}%` }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
               <strong>{`${progress}%`}</strong>
             </div>
           </div>
@@ -76,9 +130,23 @@ const PromotionCard = () => {
         <div className="mb-2">
           <h6 className="font-size-15 text-gray-2 text-center mb-3">La oferta termina en:</h6>
           <div className="js-countdown d-flex justify-content-center" data-end-date="2020/11/30" data-hours-format="%H" data-minutes-format="%M" data-seconds-format="%S">
+            
             <div className="text-lh-1">
               <div className="text-gray-2 font-size-30 bg-gray-4 py-2 px-2 rounded-sm mb-2 text-center">
-                <span className="js-cd-hours">00</span>
+                <span className="js-cd-hours">
+                  {countRegressive.remainDays}
+                </span>
+              </div>
+              <div className="text-gray-2 font-size-12 text-center">DÍAS</div>
+            </div>
+
+            <div className="mx-1 pt-1 text-gray-2 font-size-24">:</div>
+
+            <div className="text-lh-1">
+              <div className="text-gray-2 font-size-30 bg-gray-4 py-2 px-2 rounded-sm mb-2 text-center">
+                <span className="js-cd-hours">
+                  {countRegressive.remainHours}
+                </span>
               </div>
               <div className="text-gray-2 font-size-12 text-center">HORAS</div>
             </div>
@@ -87,7 +155,9 @@ const PromotionCard = () => {
 
             <div className="text-lh-1">
               <div className="text-gray-2 font-size-30 bg-gray-4 py-2 px-2 rounded-sm mb-2 text-center">
-                <span className="js-cd-minutes">00</span>
+                <span className="js-cd-minutes">
+                  {countRegressive.remainMinutes}
+                </span>
               </div>
               <div className="text-gray-2 font-size-12 text-center">MINUTOS</div>
             </div>
@@ -95,7 +165,9 @@ const PromotionCard = () => {
             <div className="mx-1 pt-1 text-gray-2 font-size-24">:</div>
             <div className="text-lh-1">
               <div className="text-gray-2 font-size-30 bg-gray-4 py-2 px-2 rounded-sm mb-2 text-center">
-                <span className="js-cd-seconds">00</span>
+                <span className="js-cd-seconds">
+                  {countRegressive.remainSeconds}
+                </span>
               </div>
               <div className="text-gray-2 font-size-12 text-center">SEGUNDOS</div>
             </div>
